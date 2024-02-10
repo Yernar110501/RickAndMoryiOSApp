fabe71600ab5a6a496179ed6865c033329d9f570
yernar <yernar.baiginzheyev@gmail.com>
2024-02-10 19:18:25 +0600

    collection view update with scroll

---
+++ RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterDetailViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Yernar Baiginzheyev on 28.01.2024.
+//
+
+import Foundation
---
+++ RickAndMorty/Views/RMFooterLoadingCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  RMFooterLoadingCollectionReusableView.swift
+//  RickAndMorty
+//
+//  Created by Yernar Baiginzheyev on 29.01.2024.
+//
+
+import UIKit
+
+class RMFooterLoadingCollectionReusableView: UICollectionReusableView {
+        
+}
